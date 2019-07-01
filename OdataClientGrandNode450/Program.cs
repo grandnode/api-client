a1868ae7eaabda4e9fbcc417c2db788632809702
Grzegorz Ociepka <grzegorzociepka09@gmail.com>
2019-07-01 15:31:51 +0200

    initial commit

---
+++ OdataClientGrandNode450/Program.cs
@@ -0,0 +1,153 @@
+﻿using Default;
+using Microsoft.OData.Client;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdataClientGrandNode450
+{
+    class Program
+    {
+        public static string UserName = "admin@yourstore.com";
+        public static string Password = "1234567";
+        public static string StoreUrl = "http://localhost:16592/";
+        public static string TOKEN;
+        private static Container container;
+        static void Main(string[] args)
+        {
+            TOKEN = WebApiServices.GenerateToken().Result;
+            container = new Container(new Uri(StoreUrl + "odata"));
+            container.BuildingRequest += OnBuildingRequest;
+
+            var product = WebApiServices.GetProduct().Result;
+            WebApiServices.UpdatePrice(product.Id,876);
+            WebApiServices.UpdateStock(product, "", 1200).Wait();
+
+            //pictures
+            byte[] binary = File.ReadAllBytes("./bill.jpg");
+            var newPicture = WebApiServices.InsertPicture(binary).Result;
+
+            ProductPictureDto newProductPicture = new ProductPictureDto();
+            newProductPicture.PictureId = newPicture.Id;
+            newProductPicture.MimeType = newPicture.MimeType;
+            newProductPicture.SeoFilename = newPicture.SeoFilename;
+            newProductPicture.AltAttribute = newPicture.AltAttribute;
+            newProductPicture.TitleAttribute = newPicture.TitleAttribute;
+
+            // WebApiServices.AddPictureToProduct(product, newProductPicture);
+
+            //5d1613b4be3bff0a8444a7d7
+
+            var updatedPicture = product.Pictures.FirstOrDefault();
+            updatedPicture.DisplayOrder = 4;
+            WebApiServices.UpdatePicture(product, updatedPicture).Wait();
+
+            //WebApiServices.RemoveProductPicture(product, "5d1613b4be3bff0a8444a7d7").Wait();
+
+            //product spec
+            ProductSpecificationAttributeDto spec = new ProductSpecificationAttributeDto();
+            spec.AllowFiltering = true;
+            spec.DisplayOrder = 1;
+            spec.ShowOnProductPage = true;
+            spec.AttributeType = SpecificationAttributeType.Option;
+            spec.SpecificationAttributeId = "5d107e372e8d1143384d957";
+            spec.SpecificationAttributeOptionId = "5d107e372e8d1143384d9580";
+            //WebApiServices.AddProductSpecification(product, spec).Wait();
+
+            ProductSpecificationAttributeDto updateSpec = new ProductSpecificationAttributeDto();
+            updateSpec.AllowFiltering = false;
+            updateSpec.DisplayOrder = 2;
+            updateSpec.ShowOnProductPage = false;
+            updateSpec.SpecificationAttributeId = "5d107e372e8d1143384d9581";
+            updateSpec.SpecificationAttributeOptionId = "5d107e372e8d1143384d9583";
+            updateSpec.Id = "";
+            //WebApiServices.UpdateProductSpecification(product, updateSpec).Wait();
+
+            //WebApiServices.RemoveProductSpecification(product, "").Wait();
+
+            //tier price
+            ProductTierPriceDto tierPrice = new ProductTierPriceDto();
+            tierPrice.Quantity = 50;
+            tierPrice.Price = 500;
+            tierPrice.StartDateTimeUtc = DateTime.UtcNow;
+            tierPrice.EndDateTimeUtc = DateTime.UtcNow.AddDays(5);
+
+           // WebApiServices.AddTierPricesToProduct(product, tierPrice).Wait();
+
+            ProductTierPriceDto updatedTierPrice = new ProductTierPriceDto();
+            updatedTierPrice.Quantity = 80;
+            updatedTierPrice.Price = 600;
+            updatedTierPrice.StartDateTimeUtc = DateTime.UtcNow.AddDays(2);
+            updatedTierPrice.EndDateTimeUtc = DateTime.UtcNow.AddDays(7);
+
+            WebApiServices.UpdateTierPrices(product, updatedTierPrice).Wait();
+            //WebApiServices.DeleteTierPrices(product,"5d161422be3bff0a8444a83a").Wait();
+
+            //manufacturer
+
+            ProductManufacturerDto manufacturer = new ProductManufacturerDto();
+            manufacturer.IsFeaturedProduct = true;
+            manufacturer.ManufacturerId = "5d107e2e2e8d1143384d9567";
+            WebApiServices.AddManufacturer(product, manufacturer).Wait();
+
+            ProductManufacturerDto updatedManufacturer = new ProductManufacturerDto();
+            updatedManufacturer.IsFeaturedProduct = false;
+            updatedManufacturer.ManufacturerId = "5d107e2e2e8d1143384d9567";
+            WebApiServices.UpdateManufacturer(product, updatedManufacturer).Wait();
+
+            //WebApiServices.DeleteManufacturer(product, "5d107e2e2e8d1143384d9567").Wait();
+
+            // categories
+            var categories = WebApiServices.GetCategories().Result.ToList();
+            var category = categories.FirstOrDefault();
+
+            //CategoryDto newCategory = new CategoryDto();
+            //newCategory.Name = "New category";
+            //newCategory.SeName = "new-category";
+            //newCategory.Published = true;
+            //newCategory.PageSize = 10;
+            //newCategory.Id = "";
+            //WebApiServices.InsertCategory(container, newCategory);
+
+            WebApiServices.UpdateCategory("5d107df92e8d1143384d9536", "new title22");
+
+            //var categoryToDelete = categories.LastOrDefault();
+            //WebApiServices.DeleteCategory(container, categoryToDelete);
+
+            //product category
+            ProductCategoryDto updatedProductCategory = product.Categories.FirstOrDefault();
+            updatedProductCategory.IsFeaturedProduct = true;
+
+            WebApiServices.UpdateProductCategoryMethod(product.Id, updatedProductCategory).Wait();
+
+            //users
+            var customer = WebApiServices.GetCustomerByEmail(UserName);
+            var role = WebApiServices.GetFirstCustomerRole(container);
+
+            var address = new AddressDto();
+            address.FirstName = "John";
+            address.LastName = "Smith";
+            address.Address1 = "Address 1";
+            address.Email = "email@test.com";
+            address.City = "New York";
+            address.PhoneNumber = "111111111";
+            address.ZipPostalCode = "33333";
+            address.CreatedOnUtc = DateTime.Now;
+            address.CountryId = "1";
+           
+
+            //WebApiServices.AssignAddressToCustomer(container, address, customer).Wait();
+            var addressToDelete = customer.Addresses.LastOrDefault();
+
+            WebApiServices.DeleteCustomerAddress(addressToDelete, customer).Wait();
+        }
+        private static void OnBuildingRequest(object sender, BuildingRequestEventArgs e)
+        {
+            e.Headers.Add("Authorization", "Bearer " + TOKEN);
+        }
+    }
+}
---
+++ OdataClientGrandNode450/WebApiServices.cs
@@ -0,0 +1,197 @@
+﻿using Default;
+using Microsoft.OData.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdataClientGrandNode450
+{
+    public class WebApiServices
+    {
+        private static string Token = GenerateToken().Result;
+        private static Default.Container container = InitContainer(Token, Program.StoreUrl);
+
+        public static void onBuildingRequest(object sender, Microsoft.OData.Client.BuildingRequestEventArgs e)
+        {
+            e.Headers.Add("Authorization", "Bearer " + Token);
+        }
+
+        public static Container InitContainer(string token, string storeurl)
+        {
+            Token = token;
+            Program.StoreUrl = storeurl;
+            container = new Default.Container(new Uri(Program.StoreUrl + "/odata/"));
+            container.BuildingRequest += onBuildingRequest;
+            container.MergeOption = MergeOption.AppendOnly;
+            return container;
+        }
+
+        public static async Task<IEnumerable<CategoryDto>> GetCategories()
+        {
+            var list = await container.Category.ExecuteAsync();
+            return list;
+        }
+        public static void InsertCategory(CategoryDto model)
+        {
+            container.AddToCategory(model);
+            container.SaveChangesAsync().Wait();
+        }
+
+        public static void DeleteCategory(CategoryDto model)
+        {
+            container.DeleteObject(model);
+            container.SaveChangesAsync().Wait();
+        }
+        public static void UpdateCategory( string oldName,string newName)
+        {
+            var test = new DataServiceCollection<CategoryDto>(container.Category.ExecuteAsync().Result);
+            test.Where(x=>x.Id == oldName).FirstOrDefault().Name = newName;
+            container.SaveChangesAsync(SaveChangesOptions.PostOnlySetProperties).Wait();
+        }
+
+        public static async Task UpdateProductCategoryMethod( string productId, ProductCategoryDto model)
+        {
+            var products = container.Product.ExecuteAsync().Result;
+            var product = products.Where(x => x.Id == productId).FirstOrDefault();
+
+            await product.UpdateProductCategory(model.CategoryId,model.IsFeaturedProduct).GetValueAsync();
+            
+        }
+        public static void UpdatePrice(string productId, decimal price)
+        {
+            var products = container.Product.ExecuteAsync().Result;
+            var product = products.Where(x => x.Id == productId).FirstOrDefault();
+            product.Price = price;
+            container.UpdateObject(product);
+            container.SaveChangesAsync().Wait();
+        }
+        public static async Task<ProductDto> GetProduct()
+        {
+            var products = await container.Product.ExecuteAsync();
+            return products.FirstOrDefault();
+        }
+
+        public static PictureDto GetPicture(string id)
+        {
+            return container.Picture.AddQueryOption("key", id).ExecuteAsync().Result.ToList().FirstOrDefault();
+        }
+        public static async Task<PictureDto> InsertPicture(byte[] binary)
+        {
+            var picture = new PictureDto();
+            picture.PictureBinary = binary;
+            picture.MimeType = "image/jpeg";
+            picture.SeoFilename = "";
+            picture.IsNew = true;
+            picture.Id = "";
+            container.AddToPicture(picture);
+            container.SaveChangesAsync().Wait();
+            return picture;
+        }
+        public static async Task UpdatePicture(ProductDto product,ProductPictureDto model)
+        {
+           await product.UpdateProductPicture(model.PictureId, model.MimeType, model.SeoFilename, model.AltAttribute, model.DisplayOrder, model.TitleAttribute).GetValueAsync();
+        }
+        public static async Task RemoveProductPicture(ProductDto product,string id)
+        {
+            product.DeleteProductPicture(id);
+        }
+        public static async Task UpdateStock(ProductDto product, string warehouseId, int stock)
+        {
+            await product.UpdateStock(warehouseId,stock).GetValueAsync();
+        }
+        public static void AddPictureToProduct(ProductDto product,ProductPictureDto PPD)
+        {
+            product.CreateProductPicture(PPD.PictureId,PPD.MimeType,PPD.SeoFilename,PPD.AltAttribute,PPD.DisplayOrder,PPD.TitleAttribute).GetValueAsync().Wait();
+        }
+
+        public static async Task AddProductSpecification(ProductDto product, ProductSpecificationAttributeDto specification)
+        {
+            await product.CreateProductSpecification("", specification.DisplayOrder, specification.CustomValue, specification.AttributeType, specification.AllowFiltering,
+                                               specification.SpecificationAttributeId, specification.ShowOnProductPage, specification.SpecificationAttributeOptionId).GetValueAsync();
+        }
+        public static async Task UpdateProductSpecification(ProductDto product, ProductSpecificationAttributeDto specification)
+        {
+            await product.UpdateProductSpecification(specification.Id, specification.DisplayOrder, specification.CustomValue, specification.AttributeType, specification.AllowFiltering,
+                                               specification.SpecificationAttributeId, specification.ShowOnProductPage, specification.SpecificationAttributeOptionId).GetValueAsync();
+        }
+        public static async Task RemoveProductSpecification(ProductDto product, string id)
+        {
+            await product.DeleteProductSpecification(id).GetValueAsync();
+        }
+
+        public static async Task AddTierPricesToProduct(ProductDto product, ProductTierPriceDto tierPrice)
+        {
+            await product.CreateProductTierPrice(tierPrice.Quantity, tierPrice.Price, "", "", tierPrice.StartDateTimeUtc, tierPrice.EndDateTimeUtc).GetValueAsync();
+        }
+        public static async Task UpdateTierPrices(ProductDto product, ProductTierPriceDto tierPrice)
+        {
+            await product.UpdateProductTierPrice(tierPrice.Id, 400, 40, "", "", tierPrice.StartDateTimeUtc, tierPrice.EndDateTimeUtc).GetValueAsync();
+        }
+        public static async Task DeleteTierPrices(ProductDto product, string id)
+        {
+            await product.DeleteProductTierPrice(id).GetValueAsync();
+        }
+
+        public static async Task AddManufacturer(ProductDto product, ProductManufacturerDto model)
+        {
+            await product.CreateProductManufacturer(model.ManufacturerId, model.IsFeaturedProduct).GetValueAsync();
+        }
+        public static async Task UpdateManufacturer(ProductDto product, ProductManufacturerDto model)
+        {
+            await product.UpdateProductManufacturer(model.ManufacturerId, model.IsFeaturedProduct).GetValueAsync();
+        }
+        public static async Task DeleteManufacturer(ProductDto product, string id)
+        {
+            await product.DeleteProductManufacturer(id).GetValueAsync();
+        }
+
+        public static CustomerDto GetCustomerByEmail( string email)
+        {
+            return container.Customer.AddQueryOption("key", email).ExecuteAsync().Result.ToList().FirstOrDefault();
+        }
+        public static CustomerRoleDto GetFirstCustomerRole(Container container)
+        {
+            return container.CustomerRole.ExecuteAsync().Result.FirstOrDefault();
+        }
+
+        public static async Task AssignAddressToCustomer(AddressDto address, CustomerDto customer)
+        {
+            await customer.AddAddress("", address.City, address.Email,address.Company, address.Address1, address.Address2, address.LastName,
+                                address.CountryId, address.FaxNumber, address.FirstName, address.VatNumber, address.PhoneNumber,
+                                address.CustomAttributes, address.CreatedOnUtc, address.ZipPostalCode, address.StateProvinceId).GetValueAsync();
+           
+        }
+        public static async Task DeleteCustomerAddress(AddressDto address, CustomerDto customer)
+        {
+            await customer.DeleteAddress(address.Id).GetValueAsync();
+        }
+ 
+        public static async Task<string> GenerateToken()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(Program.StoreUrl);
+            var credentials = new GenerateTokenModel();
+            credentials.Email = Program.UserName;
+            credentials.Password = Base64Encode(Program.Password);
+
+            var serializedJson = JsonConvert.SerializeObject(credentials);
+            var httpContent = new StringContent(serializedJson.ToString(), Encoding.UTF8, "application/json");
+            var result = await client.PostAsync("api/token/create", httpContent);
+            return result.Content.ReadAsStringAsync().Result;
+        }
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+    public class GenerateTokenModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
